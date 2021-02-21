ea90d1179ee0c4565f228ccf4d1bba056503b9ab
learnwithsona <remy@learnwithsona.com>
2021-02-20 17:38:39 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Remy Pham on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
